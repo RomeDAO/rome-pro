b6c03852dd920b941b79eaef6b7b86f0fe2029e2
Scott Cho <scottsgcho@gmail.com>
2021-12-31 04:18:31 +0900

    init rome pro contracts

---
+++ README.md
@@ -0,0 +1,14 @@
+### Compile
+npx hardhat compile
+
+### Start a local node
+npx hardhat node
+
+### Deploy to a local node
+npx hardhat run --network localhost scripts/deploy.js
+
+### Deploy to Moonbeam Alpha
+npx hardhat run --network <your-network> scripts/deploy.js
+
+### Deploy to Moonriver
+npx hardhat run --network <your-network> scripts/deploy.js
---
+++ scripts/deploy.js
@@ -0,0 +1,30 @@
+const { ethers } = require("hardhat");
+
+async function deploy() {
+    const RomeProFactoryStorage = await ethers.getContractFactory("RomeProFactoryStorage");
+    const romeProFactoryStorage = await RomeProFactoryStorage.deploy();
+    await romeProFactoryStorage.deployed()
+
+    const RPSubsidyRouter = await ethers.getContractFactory("RPSubsidyRouter");
+    const rpSubsidyRouter = await RPSubsidyRouter.deploy();
+    await rpSubsidyRouter.deployed()
+
+
+    const RomeProFactory = await ethers.getContractFactory("RomeProFactory");
+    const romeProFactory = await RomeProFactory.deploy(
+        "", //Rome treasury address
+        romeProFactoryStorage.address, 
+        rpSubsidyRouter.address, 
+        "" //RomeDao adddress
+    );
+    await romeProFactory.deployed();
+
+    console.log("RomeProFactory deployed to:", romeProFactory.address);
+}
+
+deploy()
+.then(() => process.exit(0))
+.catch((error) => {
+    console.error(error);
+    process.exit(1);
+});
