a1dcd1af761db84fb1416ae6a42f1541bd51d929
Scott Cho <scottsgcho@gmail.com>
2022-01-12 01:28:35 +0900

    generic bonding calc

--- dev-scripts/deploy.js
+++ dev-scripts/deploy.js
@@ -59,7 +59,10 @@ async function deploy() {
 
     const tierCeilings = [BigInt(1000 * 10 ** 18), BigInt(2000 * 10 ** 18)]
     const fees = [0.033 * 10 ** 6, 0.066 * 10 ** 6]
-    await romeProFactory.createBondAndTreasury(payoutToken.address, frax.address, mockDAO.address, tierCeilings, fees)
+    const hey = await romeProFactory.createBondAndTreasury(payoutToken.address, frax.address, mockDAO.address, tierCeilings, fees)
+    
+    console.log("A: ",mockDAO.address, deployer.address)
+    console.log("B: ",hey)
 }
 
 deploy()
