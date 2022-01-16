aaf7a4802d80fb47db7599a8f441f4ab2cb3f4d6
MarcusLicinius <MarcusLiciniusCrassusofRome@protonmail.com>
2022-01-16 14:01:55 +0000

    Updates

--- src/test/HND_MOVR.t.sol
+++ src/test/HND_MOVR.t.sol
@@ -14,7 +14,7 @@ import "./interfaces/IBond.sol";
 
 /* ========== CONTRACT DEPENDENCIES ========== */
 import "@openzeppelin/contracts/math/SafeMath.sol";
-import {RomeProFactory} from "../factory.sol";
+import {RomeProFactory, CustomBond, CustomTreasury} from "../factory.sol";
 import {RomeProFactoryStorage} from "../factoryStorage.sol";
 import {RPSubsidyRouter} from "../subsidy.sol";
 
@@ -55,142 +55,139 @@ contract BondTest is DSTest {
     Vm internal constant vm = Vm(0x7109709ECfa91a80626fF3989D68f67F5b1DD12D);
 
     /* ========== USER INPUTS ========= */
-    IERC20 internal PAYOUT = IERC20(0x4a436073552044D5f2f49B176853ad3Ad473d9d6);
+    IERC20 internal PAYOUT = IERC20(0x10010078a54396F62c96dF8532dc2B4847d47ED3); // HND token
 
-    IUniswapV2Pair internal PRINCIPLE = IUniswapV2Pair(0x0ceE045861051f8f5FD4e62622F87556c8e555B6);
+    IUniswapV2Pair internal PRINCIPLE = IUniswapV2Pair(0xdF1d4C921Fe6a04eF086b4191E8742eCfbDAa355); // HND/MOVR liquidity
 
     IRouter internal ROUTER = IRouter(0xAA30eF758139ae4a7f798112902Bf6d65612045f);
 
-    emit log("test");
-    // emit log("|========== USER INPUTS ==========|");
-    // emit log_named_address("Payout Token =", address(PAYOUT));
-    // emit log_named_address("Principle Token =", address(PRINCIPLE));
-    // emit log_named_address("Router", address(ROUTER));
-    // emit log("|=================================|");
+    address internal initialOwner = (0x10010078a54396F62c96dF8532dc2B4847d47ED3); // HND Multisig
 
-    // /* ========== MOVR DEPENDENCIES ========== */
-    // IERC20 internal WMOVR = IERC20(0x98878B06940aE243284CA214f92Bb71a2b032B8A);
+    /* ========== MOVR DEPENDENCIES ========== */
+    IERC20 internal WMOVR = IERC20(0x98878B06940aE243284CA214f92Bb71a2b032B8A);
 
-    // AggregatorV3Interface internal FEED = AggregatorV3Interface(0x3f8BFbDc1e79777511c00Ad8591cef888C2113C1);
+    AggregatorV3Interface internal FEED = AggregatorV3Interface(0x3f8BFbDc1e79777511c00Ad8591cef888C2113C1);
 
-    // /* ========== TESTING STATE ========== */
+    /* ========== TESTING STATE ========== */
 
-    // RomeProFactory internal factory;
+    RomeProFactory internal factory;
 
-    // RomeProFactoryStorage internal factoryStorage;
+    RomeProFactoryStorage internal factoryStorage;
 
-    // RPSubsidyRouter internal subsidy;
+    RPSubsidyRouter internal subsidy;
 
-    // uint movrPrice;
+    uint movrPrice;
 
-    // uint numberUsers = 10;
+    address internal BOND;
 
-    // BondUser[] internal user;
+    address internal TREASURY;
 
-    // function setUp() public virtual {
+    uint numberUsers = 10;
+
+    BondUser[] internal user;
+
+    function setUp() public virtual {
+
+    emit log("|==================== USER INPUTS ====================|");
+    emit log_named_address("Payout Token =", address(PAYOUT));
+    emit log_named_address("Principle Token =", address(PRINCIPLE));
+    emit log_named_address("Router", address(ROUTER));
+    emit log("|=====================================================|");
+
         
-    //     //1. Deploy Factory Storage and subsidy
-    //     factoryStorage = new RomeProFactoryStorage();
+        //1. Deploy Factory Storage and subsidy
+        factoryStorage = new RomeProFactoryStorage();
 
-    //     subsidy = new RPSubsidyRouter();
+        subsidy = new RPSubsidyRouter();
 
-    //     //2. Deploy Factory
-    //     factory = new RomeProFactory(
-    //         address(1), // Rome Dao Treasury
-    //         address(factoryStorage),
-    //         address(subsidy),
-    //         address(this) // Rome Dao Access control
-    //     );
+        //2. Deploy Factory
+        factory = new RomeProFactory(
+            address(1), // Rome Dao Treasury
+            address(factoryStorage),
+            address(subsidy),
+            address(this) // Rome Dao Access control
+        );
 
-    //     factoryStorage.setFactoryAddress(address(factory));
+        factoryStorage.setFactoryAddress(address(factory));
 
-    //     //3. CreateBond and Treasury
-    //     factory.createBondAndTreasury(
-    //         address(_payoutToken),
-    //         address(_principleToken),
-    //         address(_initialOwner),
-    //         uint[] _tierCeilings,
-    //         uint[] _fees
-    //     )
+        uint[] memory _tierCeilings = new uint[](1);
+        _tierCeilings[0] = 10*1e18;
+        uint[] memory _fees = new uint[](1);
+        _fees[0] = 33300;
 
+        //3. CreateBond and Treasury
+        (TREASURY, BOND) = factory.createBondAndTreasury(
+            address(PAYOUT),
+            address(PRINCIPLE),
+            initialOwner,
+            _tierCeilings,
+            _fees
+        );
 
+        movrPrice = uint(FEED.latestAnswer()); // Store Movr price in 8 decimals.
+        emit log_named_uint("<MOVR Price USD> ==", movrPrice.div(1e8));
 
-    //     movrPrice = uint(FEED.latestAnswer());
-    //     emit log("======================");
-    //     emit log("ROMEMOVR BONDS");
-    //     emit log("======================");
-
-    //     //2. Deploy Bonds
-    //     BOND = new ROMEMOVRBondDepository(
-    //         address( ROME ),
-    //         address( 
-    //             ROMEMOVR ),
-    //         address( TREASURY ),
-    //         address(this),
-    //         address( CALCULATOR ),
-    //         address( FEED )
-    //     );
-
-    //     for (uint i = 0; i < numberUsers; i++) {
-    //         user.push( new BondUser(BOND) );
-    //         user[i].approve(BOND.principle(), address(BOND), type(uint256).max);
-    //     }
+        for (uint i = 0; i < numberUsers; i++) {
+            user.push( new BondUser() );
+            user[i].approve(address(PRINCIPLE), address(BOND), type(uint256).max);
+        }
 
     }
 
     function testDeployment() public {
-    //     //1. set terms
-    //     uint bcv = 370;
-    //     emit log_named_uint("<|BCV|> == ", bcv);
-    //     uint minPrice = 9900;
-    //     emit log_named_uint("<|Min Price|> ==", minPrice);
-    //     uint maxPayout = 50;
-    //     emit log_named_uint("<|Max Payout|> ==", maxPayout);
+        //1. set terms
+        emit log("|==================== BOND TERMS ====================|");
+        uint bcv = 6000;
+        emit log_named_uint("<|BCV|> == ", bcv);
+        uint vestingTerm = 32000;
+        emit log_named_uint("<|Vesting Term in blocks|>", vestingTerm);
+        uint minPrice = 29040;
+        emit log_named_uint("<|Min Price|> ==", minPrice);
+        uint maxPayout = 5;
+        emit log_named_uint("<|Max Payout|> ==", maxPayout);
 
-    //     BOND.initializeBondTerms(
-    //         bcv,
-    //         32000,
-    //         minPrice,
-    //         maxPayout,
-    //         5000000000000000,
-    //         0
-    //     );   
+        vm.startPrank(initialOwner);
+        IBond(BOND).setBondTerms(IBond.PARAMETER(0),vestingTerm);
+        IBond(BOND).initializeBond(
+            bcv,
+            vestingTerm,
+            minPrice,
+            maxPayout,
+            5000000000000000,
+            1
+        );
+        vm.stopPrank();
 
-    //     //2. Prints Rome Price
-    //     // Reserve0 is Rome (9 dec), Reserve1 is Movr (18 dec).
-    //     // movPrice (8 dec)
-    //     // decimals: 18 + 8 - 9 = 17 decimals
-        
-    //     (uint reserve0, uint reserve1,) = ROMEMOVR.getReserves();
-    //     uint romePrice = reserve1.mul(movrPrice).div(reserve0);
-    //     emit log_named_uint("<MOVR Price USD> ==", movrPrice.div(1e8));
-    //     emit log_named_uint("<|Rome Price USD|> ==", romePrice.div(1e17));
+        //2. Prints HND Price       
+        (uint reserve0, uint reserve1,) = PRINCIPLE.getReserves();
+        uint tokenPrice = reserve1.mul(movrPrice).div(reserve0);
+        emit log_named_uint("<|HND Price USD|> ==", tokenPrice.div(1e8));
 
-    //     for (uint i = 0; i < numberUsers; i++) {
-    //         emit log_named_uint("deposit number",i+1);
-    //         emit log_named_uint("bond price ", BOND.bondPrice());
-    //         emit log_named_uint("bond price in USD", BOND.bondPriceInUSD().div(1e18));
-    //         address addr = address(user[i]);
+        for (uint i = 0; i < numberUsers; i++) {
+            emit log_named_uint("deposit number",i+1);
+            emit log_named_uint("bond price ", BOND.bondPrice());
+            emit log_named_uint("bond price in USD", BOND.bondPriceInUSD().div(1e18));
+            address addr = address(user[i]);
 
-    //         //4. mint Rome and Movr
-    //         setBalance(addr,50*1e18,address(WMOVR),3);
-    //         setBalance(addr,25*1e9,address(ROME),0);     
+            //4. mint Rome and Movr
+            setBalance(addr,50*1e18,address(WMOVR),3);
+            setBalance(addr,25*1e9,address(ROME),0);     
 
-    //         //5. add liquidity
-    //         uint balBefore = ROME.balanceOf(addr);
-    //         user[i].addLiquidity(address(ROUTER),address(ROME),address(WMOVR));
-    //         emit log_named_uint("LP added in ROME", 2*(balBefore.sub(ROME.balanceOf(addr))).div(1e9));
+            //5. add liquidity
+            uint balBefore = ROME.balanceOf(addr);
+            user[i].addLiquidity(address(ROUTER),address(ROME),address(WMOVR));
+            emit log_named_uint("LP added in ROME", 2*(balBefore.sub(ROME.balanceOf(addr))).div(1e9));
 
-    //         //6. purchase bonds
-    //         uint payout = user[i].deposit(ROMEMOVR.balanceOf(addr), 200000*1e11, addr);
+            //6. purchase bonds
+            uint payout = user[i].deposit(ROMEMOVR.balanceOf(addr), 200000*1e11, addr);
 
-    //         //7. Print Payout in USD
-    //         emit log_named_uint("Payout in ROME ==",payout.div(1e9));
+            //7. Print Payout in USD
+            emit log_named_uint("Payout in ROME ==",payout.div(1e9));
 
-    //         emit log("==============================");
+            emit log("==============================");
 
 
-    //     }
+        }
     }
 
     // /* ========== HELPERS ========== */
--- src/test/interfaces/IBond.sol
+++ src/test/interfaces/IBond.sol
@@ -5,4 +5,15 @@ interface IBond {
 
     function deposit(uint _amount, uint _maxPrice, address _depositor) external returns (uint);
 
+    function initializeBond( 
+        uint _controlVariable, 
+        uint _vestingTerm,
+        uint _minimumPrice,
+        uint _maxPayout,
+        uint _maxDebt,
+        uint _initialDebt
+    ) external;
+
+    enum PARAMETER { VESTING, PAYOUT, DEBT }
+    function setBondTerms ( PARAMETER _parameter, uint _input ) external;
 }
